4c898ed420f9f9525768c332db20672406a12a2e
co-bby <nkumsagoefredrick@gmail.com>
2022-09-24 21:07:02 -0400

    BinarySearch Algorithm

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,26 @@
+/* Binary Search is a searching algorithm used in a sorted array by 
+repeatedly dividing the search interval in half. Worst case scenario is O(log n)*/
 export default function bs_list(haystack: number[], needle: number): boolean {
+	// first index of an array
+	let firstIndex = 0;
+	// last index of an array
+	let lastIndex = haystack.length - 1;
+	while (firstIndex <= lastIndex) {
+		// middle index of an array
+		let middleIndex = Math.floor((firstIndex + lastIndex) / 2);
+		// if the element needle is found in the middle of haystack return true
+		if (haystack[middleIndex] === needle) {
+			return true;
+		}
+		//if the element needle is found at the left side of haystack the last index changes
+		else if (needle < haystack[middleIndex]) {
+			lastIndex = middleIndex - 1;
+		}
+		//if the element needle is found at the right side of haystack the first index changes
+		else {
+			firstIndex = middleIndex + 1;
+		}
 
+	}
+	return false;
 }
