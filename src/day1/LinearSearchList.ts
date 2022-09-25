1a3fe1b376e0a261b4438d8c908609ef94966f07
co-bby <nkumsagoefredrick@gmail.com>
2022-09-24 20:38:52 -0400

    LinearSearch Algorithm

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,13 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
+/* In linear_search we simply traverse the list completely and 
+match each element of the list with the item whose location is to be found.
+ If the match is found, then the location of the item is returned; otherwise,
+ the algorithm returns NULL.The worst case scenario is  O(N).*/
 
+export default function linear_search(haystack: number[], needle: number): boolean {
+	for (let i = 0; i < haystack.length; i++) {
+		if (haystack[i] === needle) {
+			return true;
+		}
+	}
+	return false;
 }
