38ce20bb364efdfe406f909fa3c86225577562f2
co-bby <nkumsagoefredrick@gmail.com>
2022-09-30 11:25:51 -0400

    Bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
 export default function bubble_sort(arr: number[]): void {
+	let swapped;
+	do {
+		swapped = false;
+		for (let i = 0; i < arr.length - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				let temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				swapped = true;
+			}
+		}
+	} while (swapped)
 
 }
+
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,4 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+
 }
