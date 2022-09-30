c6d4db6b367e6ccbfe92c51f8582f7cc8444a745
co-bby <nkumsagoefredrick@gmail.com>
2022-09-30 11:40:29 -0400

    added comments to BubbleSorts

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,8 +1,12 @@
+/*Bubble Sort is the simplest sorting algorithm that works by repeatedly 
+swapping the adjacent elements. Big O = O(N^2) */
+
 export default function bubble_sort(arr: number[]): void {
 	let swapped;
 	do {
 		swapped = false;
 		for (let i = 0; i < arr.length - 1; i++) {
+			// when an adjacent element is in wrong order swap
 			if (arr[i] > arr[i + 1]) {
 				let temp = arr[i];
 				arr[i] = arr[i + 1];
