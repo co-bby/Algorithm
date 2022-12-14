9dd5a34680717cb0f5c78322a6533b21b3ddb36d
co-bby <nkumsagoefredrick@gmail.com>
2022-12-14 12:25:16 -0500

    Feat: add stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,43 @@
 export default class Stack<T> {
-    public length: number;
+    // public length: number;
+    public Stck: number[];
+    private top: number;
+    public size: number
 
-    
 
-    constructor() {
+
+
+    constructor(size: number) {
+
+        this.Stck = [];
+        this.top = -1;
+        this.size = size;
+
+
     }
 
-    push(item: T): void {
+    push(item: number): void {
+        if (this.top > this.size) {
+            console.log("stack is full")
+        } else {
 
-}
-    pop(): T | undefined {
+            this.Stck[this.top++] = item;
+        }
 
-}
-    peek(): T | undefined {
 
-}
+
+
+    }
+    pop(): number | void {
+        if (this.top < 0) {
+            console.log("stack is undeflow");
+        } else {
+            this.Stck[this.top--];
+        }
+
+    }
+    peek(): number {
+        return this.Stck[this.Stck.length - 1];
+
+    }
 }
