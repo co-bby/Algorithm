1073ad6d4feb6d9809a9fd30b897c508ee817b37
co-bby <nkumsagoefredrick@gmail.com>
2022-12-13 22:30:45 -0500

    BREAKING CHANGE: change outer loop to for-loop

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,22 +1,44 @@
 /*Bubble Sort is the simplest sorting algorithm that works by repeatedly 
 swapping the adjacent elements. Big O = O(N^2) */
 
-export default function bubble_sort(arr: number[]): void {
-	let swapped;
-	do {
-		swapped = false;
-		for (let i = 0; i < arr.length - 1; i++) {
-			// if an adjacent element is in wrong order swap
-			if (arr[i] > arr[i + 1]) {
-				let temp = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = temp;
-				swapped = true;
+export default function bubble_sort(arr: number[]): void | number[] {
+	// let swapped;
+	// do {
+	// 	swapped = false;
+	// 	for (let i = 0; i < arr.length - 1; i++) {
+	// 		// if an adjacent element is in wrong order swap
+	// 		if (arr[i] > arr[i + 1]) {
+	// 			let temp = arr[i];
+	// 			arr[i] = arr[i + 1];
+	// 			arr[i + 1] = temp;
+	// 			swapped = true;
+	// 		}
+	// 	}
+	// } while (swapped)
+
+	for (let i = 0; i < arr.length - 1; i++) {
+		for (let j = 0; j < arr.length - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
 			}
 		}
-	} while (swapped)
+
+
+
+
+	}
 
 }
+
+// const arr = [3, 78, 4, 67, 2, 5]
+// bubble_sort(arr);
+// console.log(arr)
+
+
+
+
 
 
 
