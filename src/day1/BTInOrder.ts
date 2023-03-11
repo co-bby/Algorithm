8061259d5c58d05e5f32b814b3b810f851e173f8
co-bby <nkumsagoefredrick@gmail.com>
2023-03-11 11:10:19 -0500

    feat: :sparkles: In order traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,20 @@
+function walk(curr: BinaryNode<number> | null, path: number[]) {
+	if (!curr) {
+		return path;
+	}
+
+	walk(curr.left, path);
+	path.push(curr.value);
+	walk(curr.right, path);
+
+
+
+
+	// post
+	return path;
+}
+
 export default function in_order_search(head: BinaryNode<number>): number[] {
+	return walk(head, [])
 
 }
