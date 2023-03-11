a68928f0d6b3788a466344073b5beab937810458
co-bby <nkumsagoefredrick@gmail.com>
2023-03-11 11:20:29 -0500

    feat: :sparkles: add post ordr traversal

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,15 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]) {
+	if (!curr) {
+		return path;
+	}
 
+	walk(curr.left, path);
+	walk(curr.right, path);
+	path.push(curr.value);
+	return path;
+}
+
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+	return walk(head, [])
 }
