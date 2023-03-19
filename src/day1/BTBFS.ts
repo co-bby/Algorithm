271a9c47df1add6e7da59d48110fa2e63b663148
co-bby <nkumsagoefredrick@gmail.com>
2023-03-19 12:40:45 -0400

    feat: :sparkles: compare 2 binary tress

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,4 +1,5 @@
 // Breadth First Search uses the queue data structure
+// it doesn't preserve the shape of the traversal
 
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 	const q: (BinaryNode<number> | null)[] = [head];
--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,22 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
 
+
+	// structural check 
+	if (a === null && b === null) {
+		return true;
+	}
+
+	// structural check
+	if (a === null || b === null) {
+		return false;
+	}
+
+	// value check
+	if (a.value === b.value) {
+		return true;
+	}
+
+
+	return compare(a.left, b.left) && compare(a.right, b.right);
+
 }
