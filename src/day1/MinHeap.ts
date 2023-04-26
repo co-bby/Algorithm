427d9900017dba84c40c429b1ede58b1dc0e414c
co-bby <nkumsagoefredrick@gmail.com>
2023-04-25 23:36:02 -0400

    feat: :sparkles: heapifyDown/up

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,68 @@
 export default class MinHeap {
     public length: number;
+    private data: number[];
 
-    
+
 
     constructor() {
+        this.data = []
+        this.length = 0;
     }
 
     insert(value: number): void {
 
-}
+    }
     delete(): number {
 
-}
+    }
+
+    private heapifyDown(idx: number): void {
+        const lIdx = this.leftChild(idx);
+        const rIdx = this.rightChild(idx);
+
+        if (idx > this.length || lIdx >= this.length) return;
+
+        const lv = this.data[lIdx];
+        const rv = this.data[rIdx];
+        const v = this.data[idx];
+
+        if (lv > rv && lv < v) {
+            this.data[idx] = rv;
+            this.data[rIdx] = v;
+            this.heapifyDown(rIdx);
+        }
+        else if (rv < lv && v > lv) {
+            this.data[idx] = lv;
+            this.data[lIdx] = v;
+            this.heapifyDown(lIdx);
+        }
+
+    }
+    private heapifyUp(idx: number) {
+        if (idx === 0) return;
+
+        const p = this.parent(idx);
+        const parenV = this.data[p];
+        const v = this.data[idx];
+
+        if (parenV < v) {
+            this.data[p] = v;
+            this.data[idx] = parenV;
+            this.heapifyUp(p)
+        }
+
+
+
+    }
+    private parent(idx: number): number {
+        return Math.floor((idx - 1) / 2)
+    }
+
+    private leftChild(idx: number): number {
+        return 2 * idx + 1
+    }
+
+    private rightChild(idx: number): number {
+        return 2 * idx + 2
+    }
 }
